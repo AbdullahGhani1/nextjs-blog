59916b0e1b0e35e380d93754f1e7563b83ef7bc9
ghani-ds <153816946+ghani-ds@users.noreply.github.com>
2024-12-18 13:12:52 +0500

    adding Seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <h3>Drive</h3>
-    </main>
+    <>
+      <Head>
+        <title>{`Fareen Waheed's Environmental Engineering Blog`}</title>
+        <meta
+          name="description"
+          content="Explore insights and experiences from Fareen Waheed, a dedicated Environmental Engineer with extensive experience in waste management, design, and sustainability. Stay updated with the latest in environmental engineering."
+        />
+        <meta
+          name="keywords"
+          content="Environmental Engineering, Waste Management, Sustainability, NESPAK, DIC, ASHRAE, Career Insights"
+        />
+        <meta name="author" content="Fareen Waheed" />
+        <meta
+          property="og:title"
+          content="Fareen Waheed's Environmental Engineering Blog"
+        />
+        <meta
+          property="og:description"
+          content="Explore insights and experiences from Fareen Waheed, a dedicated Environmental Engineer with extensive experience in waste management, design, and sustainability. Stay updated with the latest in environmental engineering."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.fareenwaheed.com" />
+        <meta property="og:image" content="/images/profile" />
+      </Head>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h3>Drive</h3>
+      </main>
+    </>
   );
 }
